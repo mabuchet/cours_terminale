bc98df90b063ba4a9c478dcb6c29f9270b547e7c
mabuchet <mabuchet@users.noreply.github.com>
2022-02-11 15:04:25 +0100

    Update sinusoide_a_completer.py

--- battements/sinusoide_a_completer.py
+++ battements/sinusoide_a_completer.py
@@ -6,39 +6,21 @@ BUT :
 Tracer la représentation graphique d'une sinusoïde : s(t) = a*cos(omega*t + phi)
 """
 
-# Imports de bibliothèques :
+## Imports de bibliothèques :
 import numpy as np
 import matplotlib.pyplot as plt
 
-#==============================================================================
 ## Paramétrage de pyplot :
-#==============================================================================
-# Graduations en gras et en plus gros :
+plt.rc('font', weight="bold", size=12) # Graduations en gras et en plus gros
+N = 1000 # Nombre de points pour les graphiques
 
-# Activation du mode interactif :
-plt.ion()
-
-
-#==============================================================================
-## Paramètres numériques :
-#==============================================================================
-# Nombre de points pour les graphiques :
-
-#==============================================================================
 ## Paramètres physiques :
-#==============================================================================
 # Amplitude :
 a = 1. # u.a.
 
-
-#==============================================================================
 ## Grandeurs à représenter :
-#==============================================================================
 # On calcule les abscisses du graphe (ici les dates auxquelles calculer s(t) ) :
-
 
 # Et les ordonnées :
 
-#==============================================================================
 ## Représentation graphique :
-#==============================================================================
