73e3896b054b4fa02c39278bd118b9c319a3d232
mabuchet <mabuchet@users.noreply.github.com>
2022-02-11 15:00:28 +0100

    Update sinusoide.py

--- battements/sinusoide.py
+++ battements/sinusoide.py
@@ -6,20 +6,18 @@ BUT :
 Tracer la représentation graphique d'une sinusoïde : s(t) = a*cos(omega*t + phi)
 """
 
-# Imports de bibliothèques :
+## Imports de bibliothèques :
 import numpy as np
 import matplotlib.pyplot as plt
 
+## Booléen pour décider de sauvegarder les graphiques ou non :
 sauvegarder_grapiques = False
-#==============================================================================
+
 ## Paramétrage de pyplot :
-#==============================================================================
 plt.rc('font', weight="bold", size=12) # Graduations en gras et en plus gros
 N = 1000 # Nombre de points pour les graphiques
 
-#==============================================================================
 ## Paramètres physiques :
-#==============================================================================
 # Amplitude :
 a = 1. # u.a
 
@@ -35,9 +33,7 @@ omega = 2*np.pi*f
 # Et la période :
 T = 1/f
 
-#==============================================================================
 ## Grandeurs à représenter :
-#==============================================================================
 # On calcule les abscisses du graphe (ici les dates auxquelles calculer s(t) ) :
 t_min = -1.*T # En nombre de périodes, c'est le plus pertinent
 t_max = 2.*T
@@ -46,9 +42,7 @@ t = np.linspace(t_min,t_max,N)
 # Et les ordonnées :
 s = a*np.cos(omega*t+phi)
 
-#==============================================================================
 ## Représentation graphique :
-#==============================================================================
 nom_de_la_figure = "sinusoide"
 fig = plt.figure(nom_de_la_figure)
 plt.plot(t,s)
